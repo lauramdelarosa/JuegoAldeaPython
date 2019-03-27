faaab65b1f7fca0bd0695c55e2734faa54343cd4
laura De la Rosa <laura.dlr.cardona@gmailcom>
2019-03-26 23:23:35 -0500

    comentarios de la clase y refactorizacion de codigo

--- AldeaT_v4/AldeaT_v4.py
+++ AldeaT_v4/AldeaT_v4.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import random
+import sys
 
 # valida la version de python para importat las librerias de diseño
 if sys.version_info < (3, 0):
-    from Tkinter import Tk, Label, Radiobutton, PhotoImage, IntVar
+    from tkinter import Tk, Label, Radiobutton, PhotoImage, IntVar
     import tkMessageBox as messagebox
 else:
     from tkinter import Tk, Label, Radiobutton, PhotoImage, IntVar
@@ -13,31 +13,34 @@ else:
 
 
 class JuegoChozas:
+    # inicializamos los atributos de las chozas
     def __init__(self, parent):
         self.imagen_fondo = PhotoImage(file="Jungle_small_2.gif")
-        self.imagen_choza = PhotoImage(file="choza.png") 
+        self.imagen_choza = PhotoImage(file="choza.png")
 
         self.ancho_choza = 175
         self.alto_choza = 200
         self.container = parent
 
-        self.Chozas = []
+        self.chozas = []
         self.result = ""
 
         self.ocupar_chozas()
 
         self.setup()
 
+    # ocupamos las chozas con los 3 tipos de ocupantes de manera aleotoria
     def ocupar_chozas(self):
         ocupantes = ['enemigo', 'amigo', 'vacia']
-        while len(self.Chozas) < 5:
+        while len(self.chozas) < 5:
             computer_choice = random.choice(ocupantes)
-            self.Chozas.append(computer_choice)
-        print("Los ocupantes de las chozas son:", self.Chozas)
+            self.chozas.append(computer_choice)
+        print("Los ocupantes de las chozas son:", self.chozas)
 
+    # leemos el indice de la choza y almacenamos la respuesta
     def entrar_choza(self, numero_choza):
         print("Entrando en la choza #:", numero_choza)
-        ocupante_choza = self.Chozas[numero_choza-1]
+        ocupante_choza = self.chozas[numero_choza - 1]
         print("El ocupante de la choza es: ", ocupante_choza)
 
         if ocupante_choza == 'enemigo':
@@ -52,6 +55,7 @@ class JuegoChozas:
 
         self.anunciar_ganador(self.result)
 
+    # creamos los widgets entre ellos textos y radiobuttons
     def crear_widgets(self):
 
         self.var = IntVar()
@@ -74,10 +78,12 @@ class JuegoChozas:
         self.r4 = Radiobutton(self.container, r_btn_config, value=4)
         self.r5 = Radiobutton(self.container, r_btn_config, value=5)
 
+    # configuramos la ventana tkinter
     def setup(self):
         self.crear_widgets()
         self.setup_layout()
 
+    # creamos en el contenedor una tabla y ubicamos los radiobutton alli
     def setup_layout(self):
         self.container.grid_rowconfigure(1, weight=1)
         self.container.grid_columnconfigure(0, weight=1)
@@ -87,22 +93,25 @@ class JuegoChozas:
         self.r1.grid(row=1, column=0)
         self.r2.grid(row=1, column=2)
         self.r3.grid(row=1, column=4)
-        self.r4.grid(row=4, column=2)
-        self.r5.grid(row=4, column=0)
+        self.r4.grid(row=4, column=0)
+        self.r5.grid(row=4, column=2)
 
+    # se muestra un mensaje en un popup con la informacion que necesitamos mostrar
     def anunciar_ganador(self, data):
         messagebox.showinfo("¡Atención!", message=data)
 
-    # Handle Events
+    # cuando hacen click en el radiobutton obtenemos el numero de la choza que esogio
     def radio_btn_pressed(self):
         self.entrar_choza(self.var.get())
 
+
+# inicalizamos la ventana con tkinter
 if __name__ == "__main__":
     mainWindow = Tk()
-    WIDTH = 1280
-    HEIGHT = 700
-    mainWindow.geometry("%sx%s" % (WIDTH, HEIGHT))
+    width = 1280
+    height = 700
+    mainWindow.geometry("%sx%s" % (width, height))
     mainWindow.resizable(0, 0)
-    mainWindow.title("Ataca a los orcos V 2.0.0 - El Videojuego")
+    mainWindow.title("Ataca a los hombre lobo AldeaT - El Videojuego")
     game_app = JuegoChozas(mainWindow)
     mainWindow.mainloop()
